48b1df10ebb2e030e9403d4b9ec3414d5b2c7a13
DevCarloss <=>
2023-03-31 08:52:32 -0300

    Update

--- README.md
+++ README.md
@@ -1,18 +1,29 @@
 # BotGPT
-O Bot do Discord que utiliza a API do OpenAI é uma ferramenta poderosa que pode ajudá-lo em diversas tarefas relacionadas à programação
+O Bot do Discord que utiliza a API do OpenAI é uma ferramenta poderosa que pode ajudá-lo em diversas tarefas
 # O que é Chat OpenAI?
 O Chat GPT é uma ferramenta conversacional online que pode ser acessada por meio do seu site oficial: chat.openai.com. Ele funciona a partir de uma base de conhecimento atualizada que permite decodificar palavras para oferecer respostas textuais às pessoas.
 # Tutorial de Instalação
 1. Instale todos os modulos do script:<br>
 pip install modules<br>
 2. Crie um arquivo .env no mesmo diretório do arquivo .py com os seguintes dados:<br>
-APITOKEN= "Token da API que voce deve solicitar no site da OpenAI"<br>
-BOTTOKEN= "Token da aplicação discord (BOT)"<br>
+OPENAI_KEY = "KEY Da OpenAI"
+BOTTOKEN = "Token Do Seu Bot Discord"
+OPENAI_ORGANIZATION = "Organização da sua API OPENAI"
 3. Agora so executar a Aplicação<br>
 # Comandos
-/completion<br>
-Para gerar e editar textos<br>
-Mais comandos em breve
+/ia_getimage<br>
+Para Gerar Imagem Usando Inteligência Artificial Da OPENAI<br>
+/ia_chatcompletion<br>
+Com Esse Comando Voce Pode:<br>
+Redigir um e-mail ou outro texto<br>
+Escrever código Python<br>
+Responder a perguntas sobre um conjunto de documentos<br>
+Criar agentes de conversação<br>
+Dê ao seu software uma interface de linguagem natural<br>
+Tutor em uma variedade de assuntos<br>
+Traduzir idiomas<br>
+Simular personagens para videogames e muito mais<br>
+
 
 
 
--- bot.py
+++ bot.py
@@ -1,65 +1,92 @@
 import discord
-import json
 from discord.ext import commands
-from discord import app_commands
-import dotenv
 import os
-import requests
-dotenv.load_dotenv(dotenv.find_dotenv("env"))
+import openai
+import dotenv
+dotenv.load_dotenv(dotenv.find_dotenv('.env'))
 
-# Bot GPT Discord
+# OPENAI Credentials
+openai.organization = os.getenv("OPENAI_ORGANIZATION")
+openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.Model.list()
 
+# Bot GPT 2.0 OPENAI
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='/',intents=intents)
+client = commands.Bot(command_prefix='/',intents=intents)
 
-@bot.command()
+# First Command
+# Image Generation
 
-async def completion(ctx,*,arg1):
-    url = "https://api.openai.com/v1/completions"
-    headers = {
-     "Content-Type": "application/json",
-     "Authorization": "Bearer " + os.getenv("APITOKEN"),
-     "Accept": "application/json"
-    }
-    data = json.dumps({
-     "model": "text-davinci-003",
-     "prompt": str(arg1),
-     "temperature": 0.5, 
-     "max_tokens": 2048
-    })
+# Learn how to generate or manipulate images with our DALL·E models
+# Introduction
 
-    req_API = requests.post(url,headers=headers,data=data).json()
+# The Images API provides three methods for interacting with images:
+# Creating images from scratch based on a text prompt
+# Creating edits of an existing image based on a new text prompt
+# Creating variations of an existing image
+# This guide covers the basics of using these three API endpoints with useful code samples. To see them in action, check out our DALL·E preview app.
 
+@client.command()
+async def ia_getimage(ctx,*,arg1):
     try:
-        for texto in req_API['choices']:
-            embed = discord.Embed(
-                title = "**"+ "BotGPT" + "**",
-                description =   "\n" + "\n" + texto['text'],
-                color = discord.Color.from_rgb(183,207,117)
-            )
-            embed.set_footer(text="OpenAI")
-            await ctx.send(embed=embed)
+        response = openai.Image.create(
+            prompt = str(arg1),
+            n = 1,
+            size = "1024x1024"
+        )
+        imageUrl = response['data'][0]['url']
+        embed = discord.Embed(
+            title = "**ChatGPT Image Generator**",
+            description = "Prompt: " + str(arg1),
+            color = discord.Color.from_rgb(40,256,163)
+        )
+        embed.set_image(url=imageUrl)
+        embed.set_footer(text="OPENAI" + '\n' + 'Discord Bot Developer: @DevCarloss')
+        await ctx.send(embed=embed)
+
     except:
         embed = discord.Embed(
-            title = "**" + "Bot GPT" + "**",
-            description= "\n" + "\n" + "Desculpe! Houve um erro a o tentar processar a resposta",
-            color = discord.Color.from_rgb(183,207,117)
+            title = "**Error Generating Response Try Again**",
+            color = discord.Color.from_rgb(255,255,255)
         )
-        embed.set_footer(text="OpenAI")
-        await  ctx.send(embed=embed)
+        embed.set_footer(text="OPENAI" + '\n' + 'Discord Bot Developer: @DevCarloss')
+        await ctx.send(embed=embed)
 
-@bot.command()
+# Second Command
+# Chat Completions
 
-async def helper(ctx):
-    titulo = 'BotGPT'
-    descricao = 'É um bot desenvolvido na linguagem python' + '\n' + 'que ultiliza a API da OpenAI para obter as resposta dos comandos' + '\n' + '\n'  + 'Comandos' + '\n' + '\n' +  '**' + '``/completion``' + '**' + '\n' +  'Para Gerar e Editar Textos'
-    embed = discord.Embed(
-        title = "**" +  titulo + '**',
-        description = descricao,
-        color = discord.Color.from_rgb(255,255,255)
-    )
-    embed.set_footer(text="OpenAI")
-    await ctx.send(embed=embed)
+# Using the OpenAI Chat API, you can build your own applications with gpt-3.5-turbo and gpt-4 to do things like:
+# Draft an email or other piece of writing
+# Write Python code
+# Answer questions about a set of documents
+# Create conversational agents
+# Give your software a natural language interface
+# Tutor in a range of subjects
+# Translate languages
+# Simulate characters for video games and much more
+# This guide explains how to make an API call for chat-based language models and shares tips for getting good results. You can also experiment with the new chat format in the OpenAI Playground.
+
+#Introduction
+#Chat models take a series of messages as input, and return a model-generated message as output.
+#Although the chat format is designed to make multi-turn conversations easy, it’s just as useful for single-turn tasks without any conversations (such as those previously served by instruction following models like text-davinci-003).
+
+@client.command()
+async def ia_chatcompletion(ctx,*,arg1):
+    
+        response = openai.ChatCompletion.create(
+           model="gpt-3.5-turbo",
+           messages=[
+              {"role": "user", "content": str(arg1)},
+           ]
+        )
+        completion_result = response['choices'][0]['message']['content']
+        embed = discord.Embed(
+            title = "**ChatGPT Chat Completions**",
+            description = '\n' + str(completion_result),
+            color = discord.Color.from_rgb(197,240,55)
+        )
+        embed.set_footer(text="OPENAI" + '\n' + 'Discord Bot Developer: @DevCarloss')
+        await ctx.send(embed=embed)
 
 if __name__ == '__main__':
-    bot.run(os.getenv("BOTTOKEN"))
+     client.run(os.getenv("BOTTOKEN"))
